df5aeca042aaa17ac3de30350be783fbca2d62bc
Brandon Warner <warner.brandon.a@gmail.com>
2020-02-07 10:32:17 -0600

    Add functions in controller. export module. fix server errors

---
+++ server/controller.js
@@ -0,0 +1,26 @@
+getProducts = (req, res) => {
+
+}
+
+getProduct = (req, res) => {
+
+}
+
+createProduct = (req, res) => {
+
+}
+
+updateProduct = (req, res) => {
+
+}
+
+deleteProduct = (req, res) => {
+
+}
+module.exports = {
+    getProducts,
+    getProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct
+}
--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const massive = require('massive');
 const dotenv = require('dotenv');
 dotenv.config();
 
+var controller = require('./controller');
+
 let { SERVER_PORT, CONNECTION_STRING } =  process.env;
 
 app.listen(SERVER_PORT, () => console.log(`Party on, Wayne!`));
@@ -20,10 +22,14 @@ massive(CONNECTION_STRING)
 } )
 .catch( () => console.log('Looks like you didn\'t connect to the database properly. Try again.') );
 
+// Import from Controller
+
+var { getProducts, getProduct, createProduct, updateProduct, deleteProduct } = controller;
+
 // End Points
 
 app.get('/api/products', getProducts);          // Read (All)
 app.post('/api/products', createProduct);       // Create
 app.get('/api/products/:id', getProduct);       // Read
-app.push('/api/products/:id', updateProduct);   // Update
+app.put('/api/products/:id', updateProduct);   // Update
 app.delete('/api/products/:id', deleteProduct); // Delete
